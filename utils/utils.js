5e06037716df810fd5064410f89e600dc0e17799
Toomeme <jtoom238@gmail.com>
2024-04-25 14:22:09 -0400

    format for readability

--- utils/utils.js
+++ utils/utils.js
@@ -3,79 +3,77 @@ const { response } = require('express');
 //function to remove blank entries
 const removeNullUndefined = obj => Object.entries(obj).reduce((a, [k, v]) => (v == null ? a : (a[k] = v, a)), {});
 //function to add new k/v pairs to an object
-const add = (obj, k, v) => Object.assign(obj, obj[k] 
-? { [k]: [].concat(obj[k], v) } 
-: { [k]: v });
+const add = (obj, k, v) => Object.assign(obj, obj[k]
+    ? { [k]: [].concat(obj[k], v) }
+    : { [k]: v });
 
 const query = require('./queryAPI');
 // In case you don't have top level await yet
 async function start(baseURL) {
-  const sharePointSite = await query.queryGraphApi(baseURL);
-  //console.log(sharePointSite);
-  return sharePointSite;
+    const sharePointSite = await query.queryGraphApi(baseURL);
+    //console.log(sharePointSite);
+    return sharePointSite;
 }
 
-const getDaysPerMonth = (month,year) =>
-{
+const getDaysPerMonth = (month, year) => {
     days = new Date(year, month, 0).getDate();
     return parseInt(days);
 };
 
-function breakdownToDays(ob,month){
+function breakdownToDays(ob, month) {
 
     // Create our number formatter.
     const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
+        style: 'currency',
+        currency: 'USD',
     });
     //get keys from the object we passed in
     keysToChange = Object.keys(ob);
     //modify any key that contains a dollar amount
     keysToChange.forEach(key => {
         if (ob[key]) {
-          valueArray = ob[key];
-          valueArray.forEach((number, index) =>{
-            if (number.includes('$')){
-                //strip any character that isnt a number or decimal point
-                var string = number.replace(/[^0-9.]/g,'');
-                //parse into an int
-                var backToNumber = parseInt(string);
-                //divide by days in the month we passed in, rounded to nearest tenth, then formatted to USD
-                var dollarAmount = formatter.format(Math.round((backToNumber / getDaysPerMonth(month,2024)) * 100.0) / 100.0);
-                valueArray[index] = dollarAmount;
-            }
-          });
+            valueArray = ob[key];
+            valueArray.forEach((number, index) => {
+                if (number.includes('$')) {
+                    //strip any character that isnt a number or decimal point
+                    var string = number.replace(/[^0-9.]/g, '');
+                    //parse into an int
+                    var backToNumber = parseInt(string);
+                    //divide by days in the month we passed in, rounded to nearest tenth, then formatted to USD
+                    var dollarAmount = formatter.format(Math.round((backToNumber / getDaysPerMonth(month, 2024)) * 100.0) / 100.0);
+                    valueArray[index] = dollarAmount;
+                }
+            });
         }
-      });
-      var daysPerMonth = getDaysPerMonth(month,2024);
-      var entryLength = -1;
-      for (const [key, value] of Object.entries(ob)) {
+    });
+    var daysPerMonth = getDaysPerMonth(month, 2024);
+    var entryLength = -1;
+    for (const [key, value] of Object.entries(ob)) {
         entryLength = value.length;
-        for ( var i = 0; i < daysPerMonth; i++ ) {
+        for (var i = 0; i < daysPerMonth; i++) {
             var newKey = `${key}-${i + 1}`;
             add(ob, newKey, value);
         }
         delete ob[key];
-      };
-      if (entryLength > 0)
-      {
-        for ( var i = 0; i < daysPerMonth; i++ ) {
-        //add dates
-        var newDate = `Date-${i + 1}`;
-        let date = `${month}/${i + 1}/2024`;
-        for ( var k = 0; k < entryLength; k++ ) {
-        add(ob, newDate, date)}
+    };
+    if (entryLength > 0) {
+        for (var i = 0; i < daysPerMonth; i++) {
+            //add dates
+            var newDate = `Date-${i + 1}`;
+            let date = `${month}/${i + 1}/2024`;
+            for (var k = 0; k < entryLength; k++) {
+                add(ob, newDate, date)
+            }
         }
-      }
-      return ob;
+    };
+    return ob;
 
 }
 
-function createKeyValuePairs(sheet,table)
-{
-    /*let apiData = {};
+function createKeyValuePairs(sheet, table) {
+    let apiData = {};
     let baseURL = `/sites/a88811de-8894-4bd9-9931-73feea44227b/drives/b!3hGIqJSI2UuZMXP-6kQie5JM6iWqaKZBkLyQBNo792_M0fOy6sZmSYcsM2DcKSp5/items/016J73M3IDZ32GHELGJFH2ARVWF7KWGBXJ/workbook/worksheets/{${sheet}}/tables/{${table}}/range?$select=text`;
-    start(baseURL).then((response) => {apiData = response; console.log(apiData)});*/
+    start(baseURL).then((response) => { apiData = response; console.log(apiData) });
     let data = [
         [
             "Line Item",
@@ -212,36 +210,36 @@ function createKeyValuePairs(sheet,table)
             "$1,041,839.46",
             "100%"
         ]
-    ]
-            //define new object
-            const o = {}
+    ];
+    //define new object
+    const o = {};
 
-            //define arrays from data, and init results array
-            var keyArray = data[0],
-            result = [],
-            slicedData = data.slice(1),
-            dataFlat = slicedData.flat(),
-            length = data.length - 1;
+    //define arrays from data, and init results array
+    var keyArray = data[0],
+        result = [],
+        slicedData = data.slice(1),
+        dataFlat = slicedData.flat(),
+        length = data.length - 1;
 
-            //take as many elements of the flattened array as there are object keys, offset by current position, then push to results
-            for (var row = 0; row < length; row++) {
-                    var array2 = dataFlat.slice(keyArray.length * (row + 1));
-                    var array2Sliced = array2.slice(0,keyArray.length);
-                    result.push(array2Sliced);
+    //take as many elements of the flattened array as there are object keys, offset by current position, then push to results
+    for (var row = 0; row < length; row++) {
+        var array2 = dataFlat.slice(keyArray.length * (row + 1));
+        var array2Sliced = array2.slice(0, keyArray.length);
+        result.push(array2Sliced);
+    }
+    //for as many rows as there are in the table (results length), add a k/v pair to the object as long as it's not undefined
+    result.forEach((element) => {
+        for (var i = 0; i < keyArray.length; i++) {
+            if (element[i] !== undefined) {
+                add(o, keyArray[i].toString(), element[i]);
             }
-            //for as many rows as there are in the table (results length), add a k/v pair to the object as long as it's not undefined
-            result.forEach((element) => {
-              for ( var i = 0; i < keyArray.length; i++ ) {
-                if (element[i] !== undefined){
-                    add(o, keyArray[i].toString(), element[i]);
-                }
-              }
-            }
-            );
-            //trim empty entries and output our object
-            var newObj = removeNullUndefined(o);
-            
-            return newObj;
+        }
+    }
+    );
+    //trim empty entries and output our object
+    var newObj = removeNullUndefined(o);
+
+    return newObj;
 };
 
-module.exports = {getDaysPerMonth,createKeyValuePairs,breakdownToDays};
+module.exports = { getDaysPerMonth, createKeyValuePairs, breakdownToDays };
