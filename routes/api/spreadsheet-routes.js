bfa2f4aae44e5afe7d9d01c0bcb2c27641c6659c
Toomeme <jtoom238@gmail.com>
2024-04-30 13:59:29 -0400

    add route for non-date

--- routes/api/spreadsheet-routes.js
+++ routes/api/spreadsheet-routes.js
@@ -9,6 +9,14 @@ router.get('/:item', async (req, res) => {
   const objResponse = await utils.createKeyValuePairs(itemID,"{00000000-0001-0000-0100-000000000000}","{00000000-000C-0000-FFFF-FFFF0C000000}",0,0);
   res.send(objResponse);
 })
+router.get('/:item/:table/:sheet', async (req, res) => {
+  //get workbook by table and sheet id
+  const itemID = req.params.item;
+  const tableID = req.params.table;
+  const sheetID = req.params.sheet;
+  const objResponse = await utils.createKeyValuePairs(itemID,tableID,sheetID,0,0);
+  res.send(objResponse);
+});
 
 router.get('/:item/:table/:sheet/:id', async (req, res) => {
   //get workbook by table and sheet id, then use date id to divide costs to daily
@@ -18,6 +26,5 @@ router.get('/:item/:table/:sheet/:id', async (req, res) => {
   const dateID = req.params.id;
   const objResponse = await utils.createKeyValuePairs(itemID,tableID,sheetID,1,dateID);
   res.send(objResponse);
-
 });
 module.exports = router;
